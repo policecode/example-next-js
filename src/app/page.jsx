4d55b41e2605201d5d9564e0da553d98d3b8ee9d
Nguyễn Hoàng Đạt <dat01655993202@gmail.com>
2025-07-09 23:54:32 +0700

    update

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -8,9 +8,32 @@ export const metadata = {
 };
 export default function Home() {
   return (
-    <div className="content-wrapper max-w-screen-xl text-base mx-auto px-8 bg-slate-200">
+    <div className="content-wrapper max-w-screen-xl text-base mx-auto">
       <Header />
-      
+      <div className="py-10 bg-gray-200 text-[#4a474c]">
+        <div className="flex flex-col justify-center items-center">
+          <h1 className="text-4xl">YouTube Video Downloader</h1>
+          <div className="my-6 italic">
+            download youtube videos, convert Youtube video to mp3/mp4 for free
+          </div>
+        </div>
+
+        <div className="fvn-form flex justify-center items-center gap-5">
+          <div className="fvn-form-item max-w-2xl">
+            <input
+              type="email"
+              placeholder="https://www.youtube.com/watch?v=H9KESr_ceJs&t=9s"
+              className="px-6 py-4 text-[#4a474c] w-max bg-inherit border-[2px] border-[#4e5a87] rounded-lg outline-none leading-5 hover:border-sky-500 focus:border-sky-500 duration-500 placeholder:text-xs placeholder:tracking-widest placeholder:font-semibold" />
+          </div>
+          <div className="fvn-form-item">
+            <button
+              type="submit"
+              className="fvn-button-1 bg-blue-400 text-white rounded-lg w-max">
+              Download
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
--- src/components/me/header.jsx
+++ src/components/me/header.jsx
@@ -1,32 +1,37 @@
-'use client'
+"use client";
 import * as React from "react";
 import Link from "next/link";
-import { useState } from 'react';
-import clsx from 'clsx'
+import { useState } from "react";
+import clsx from "clsx";
 export default function Header({ children }) {
- 
   const [hiddenMenu, setHiddenMenu] = useState(true);
 
   const handleShowMenu = (e) => {
     console.log(e.target);
-    
-    setHiddenMenu(!hiddenMenu)
-  }
+    setHiddenMenu(!hiddenMenu);
+  };
   return (
-    <header className="py-6 mx-10">
+    <header className="py-6 bg-[#4e5a87]">
       <nav className="flex flex-row justify-between items-center">
-        <Link href="/" className="basis-2/6 text-center text-xl font-semibold cursor-pointer">
+        <Link
+          href="/"
+          className="basis-1/6 text-center text-xl text-white font-semibold cursor-pointer"
+        >
           MediaSocial
         </Link>
-        <ul className={`lg:basis-3/6 lg:flex lg:items-center lg:justify-end lg:gap-8 uppercase text-sm text-gray-500 font-medium ${clsx({'hidden':hiddenMenu})} fvn-mobile-top-menu-item`}>
+        <ul
+          className={`lg:basis-4/6 lg:flex lg:items-center lg:justify-end lg:gap-8 uppercase text-sm  text-white font-medium ${clsx(
+            { hidden: hiddenMenu }
+          )} fvn-mobile-top-menu-item`}
+        >
           <li className="fvn-top-menu-item">
             <Link href="#" className="">
-              Home
+              Youtube Downloader
             </Link>
           </li>
           <li className="fvn-top-menu-item">
             <Link href="#" className="">
-              Our Product
+              Product
             </Link>
           </li>
           <li className="fvn-top-menu-item">
@@ -34,54 +39,36 @@ export default function Header({ children }) {
               Blog
             </Link>
           </li>
-          <li className="fvn-top-menu-item">
-            <Link href="#" className="">
-              About
-            </Link>
-          </li>
-          <li className="fvn-top-menu-item">
-            <Link href="#" className="">
-              Contact
-            </Link>
-          </li>
-          <li className="fvn-top-menu-item">
-            <Link href="#" className="">
-              StyleGuide
-            </Link>
-          </li>
-          <li className="fvn-top-menu-item">
-            <Link href="#" className="" target="_blank">
-              Hoang Dat Police
-            </Link>
-          </li>
         </ul>
 
-        <ul className="basis-3/6 lg:basis-1/6 flex justify-end lg:justify-start items-center ml-8 sm:ml-16 uppercase text-sm text-gray-500 font-medium">
-          <li className="fvn-top-menu-item">
+        <ul className="basis-3/6 lg:basis-1/6 flex justify-end lg:justify-start items-center ml-8 sm:ml-16 uppercase text-sm text-white font-medium">
+          <li className="">
             <Link href="#" className="flex items-center">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
                 viewBox="0 0 24 24"
-                strokeWidth={1.5}
+                stroke-width="1.5"
                 stroke="currentColor"
-                className="fvn-icon"
+                class="size-6"
               >
                 <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M15.75 10.5V6a3.75 3.75 0 1 0-7.5 0v4.5m11.356-1.993 1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 0 1-1.12-1.243l1.264-12A1.125 1.125 0 0 1 5.513 7.5h12.974c.576 0 1.059.435 1.119 1.007ZM8.625 10.5a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm7.5 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="m9 12.75 3 3m0 0 3-3m-3 3v-7.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
                 />
               </svg>
 
-              <span className="mx-2">Cart</span>
               <span className="fvn-badge-circle bg-orange-400 text-white">
                 99+
               </span>
             </Link>
           </li>
         </ul>
-        <div onClick={handleShowMenu} className="basis-1/6 lg:hidden flex  cursor-pointer px-2 sm:px-4">
+        <div
+          onClick={handleShowMenu}
+          className="basis-1/6 lg:hidden flex text-white cursor-pointer px-2 sm:px-4"
+        >
           <svg
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
